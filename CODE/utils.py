9f680a386a2252e2d1e17f650ca68d5a71d877dc
NgocDuy3112 <duynguyen31122002@gmail.com>
2023-07-03 09:26:26 +0700

    Add a message

---
+++ CODE/utils.py
@@ -0,0 +1,198 @@
+import numpy as np
+from simplex import *
+
+def convert_string_to_float(string):
+    try:
+        if string.find('/') != -1:
+            elements = string.split('/')
+            return float(elements[0]) / float(elements[1])
+        else:
+            return float(string)
+    except:
+        raise Exception('Invalid number format')
+    
+
+class LPProblem():
+    def __init__(self, n_variables, n_constraints, n_range_constraints):
+        self.n_variables = n_variables
+        self.n_constraints = n_constraints
+        self.n_range_constraints = n_range_constraints
+        self.variables = ['x' + str(i) for i in range(1, n_variables + 1)]
+        self.constraints = []
+        self.range_constraints = []
+        self.pos_variables = set()
+        self.neg_variables = set()
+        self.unsigned_variables = set()
+        self.objective = None
+        self.type = None
+        self.A = None
+        self.b = None
+        self.c = None
+        self.solver = None
+
+    def __transform_objective__(self):
+        elements = self.objective.split()
+        problem_type = elements[0]
+        c = []
+        var_index = -1
+        self.type = 1 if problem_type == 'min' else -1
+        for i in range(2, len(elements), 2):
+            var_index += 1
+            if elements[i - 1][0] == '-':
+                c.append(-1 * convert_string_to_float(elements[i][0])) if elements[i][0] != 'x' else c.append(-1.0)
+            elif elements[i - 1][0] == '+':
+                c.append(convert_string_to_float(elements[i][0])) if elements[i][0] != 'x' else c.append(1.0)
+            else:
+                raise Exception('Invalid objective function')
+            assert elements[i][len(elements[i]) - 2:] == self.variables[var_index], 'Invalid objective function'
+        self.c = np.array(c) if problem_type == 'min' else -1 * np.array(c)
+        return self
+    
+    def __transform_constraints__(self):
+        for constraint_str in self.constraints:
+            elements = constraint_str.split()
+            A_row = [0] * self.n_variables
+            var_index = -1
+            if elements[-2] == '<=':
+                for i in range(1, len(elements) - 2, 2):
+                    var_index = self.variables.index(elements[i][len(elements[i]) - 2:])
+                    if elements[i - 1][0] == '-':
+                        A_row[var_index] = (-1 * convert_string_to_float(elements[i][0])) if elements[i][0] != 'x' else -1.0
+                    elif elements[i - 1][0] == '+':
+                        A_row[var_index] = (convert_string_to_float(elements[i][0])) if elements[i][0] != 'x' else 1.0
+                self.b = np.array([convert_string_to_float(elements[-1])]) if self.b is None else np.append(self.b, convert_string_to_float(elements[-1]))
+            elif elements[-2] == '>=':
+                for i in range(1, len(elements) - 2, 2):
+                    var_index = self.variables.index(elements[i][len(elements[i]) - 2:])
+                    if elements[i - 1][0] == '-':
+                        A_row[var_index] = (convert_string_to_float(elements[i][0])) if elements[i][0] != 'x' else 1.0
+                    elif elements[i - 1][0] == '+':
+                        A_row[var_index] = (-1 * convert_string_to_float(elements[i][0])) if elements[i][0] != 'x' else -1.0
+                self.b = np.array([-1 * convert_string_to_float(elements[-1])]) if self.b is None else np.append(self.b, -1 * convert_string_to_float(elements[-1]))
+            self.A = np.reshape(np.array(A_row), (1, len(A_row))) if self.A is None else np.vstack((self.A, np.array(A_row)))
+            
+        return self
+    
+    def __transform_range_constraints__(self):
+        for constraint in self.range_constraints:
+            elements = constraint.split()
+            if elements[1] == '<=':
+                if elements[2] == '0':
+                    self.neg_variables.add(elements[0])
+                    idx = self.variables.index(elements[0])
+                    self.A[:, idx] = -1 * self.A[:, idx]
+                    self.c[idx] = -1 * self.c[idx]
+                else:
+                    A_row = np.zeros(self.n_variables)
+                    A_row[self.variables.index(elements[0])] = 1
+                    self.A = np.vstack((self.A, A_row))
+                    self.b = np.append(self.b, convert_string_to_float(elements[2]))
+                    self.n_constraints += 1
+            elif elements[1] == '>=':
+                if elements[2] == '0':
+                    self.pos_variables.add(elements[0])
+                if elements[2] != '0':
+                    A_row = np.zeros(self.n_variables)
+                    A_row[self.variables.index(elements[0])] = -1
+                    self.A = np.vstack((self.A, A_row))
+                    self.b = np.append(self.b, -1 * convert_string_to_float(elements[2]))
+                    self.n_constraints += 1
+        self.unsigned_variables = set(self.variables) - self.pos_variables - self.neg_variables
+        for variable in self.unsigned_variables:
+            idx = self.variables.index(variable)
+            self.variables[idx] = variable + '+'
+            self.variables.insert(idx + 1, variable + '-')
+            self.A = np.insert(self.A, idx + 1, -1 * self.A[:, idx], axis=1)
+            self.c = np.insert(self.c, idx + 1, -1 * self.c[idx])
+        return self
+
+    def __get_problem__(self):
+        self.__transform_objective__()
+        self.__transform_constraints__()
+        self.__transform_range_constraints__()
+        return self
+    
+    def set_objective(self, objective_str):
+        self.objective = objective_str
+        return self
+
+    def add_constraint(self, constraint_str):
+        if constraint_str == '':
+            return self
+        elements = constraint_str.split()
+        if elements[-2] == '<=' or elements[-2] == '>=':
+            self.constraints.append(constraint_str)
+        else:
+            self.constraints.append(constraint_str.replace('=', '<='))
+            self.constraints.append(constraint_str.replace('=', '>='))
+        return self
+    
+    def add_range_constraint(self, constraint_str):
+        self.range_constraints.append(constraint_str)
+        return self
+    
+    def solve(self):
+        self.__get_problem__()
+        if np.any(self.b < 0):
+            self.solver = TwoPhaseSimplexSolver(self.A, self.b, self.c)
+        elif np.any(self.b == 0):
+            self.solver = BlandSimplexSolver(self.A, self.b, self.c)
+        else:
+            self.solver = DantzigSimplexSolver(self.A, self.b, self.c)
+        self.solver.solve()
+        return self
+    
+    def get_solution(self):
+        solution = self.solver.get_solution()
+        if solution is None:
+            return None
+        variables = self.variables
+        real_variables = set()
+        for variable in variables:
+            if variable[-1] == '+' or variable[-1] == '-':
+                real_variables.add(variable[:-1])
+            else:
+                real_variables.add(variable)
+        real_variables = list(real_variables)
+        real_variables.sort()
+        real_solution = []
+        for i in range(len(variables)):
+            if variables[i][-1] == '+':
+                real_solution.append(solution[i] - solution[i + 1])
+            elif variables[i][-1] == '-':
+                continue
+            else:
+                if variables[i] in self.pos_variables:
+                    real_solution.append(solution[i])
+                elif variables[i] in self.neg_variables:
+                    real_solution.append(-1 * solution[i])
+                else:
+                    real_solution = real_solution
+        return dict(zip(real_variables, real_solution))
+
+    def get_optimal_value(self):
+        if self.solver.get_optimal_value() is None:
+            return None
+        return self.solver.get_optimal_value() * self.type
+    
+    def get_status(self):
+        return self.solver.get_status()
+
+    
+if __name__ == "__main__":
+    lp_problem = LPProblem(4, 2, 5)
+    lp_problem.set_objective("min - 10x1 + x2 + 9x3 + 24x4")
+    lp_problem.add_constraint("+ 0.5x1 - 5.5x2 - 2.5x3 + 9x4 <= 0")
+    lp_problem.add_constraint("+ 0.5x1 - 1.5x2 - 0.5x3 + 1x4 <= 0")
+    lp_problem.add_range_constraint("x1 >= 0")
+    lp_problem.add_range_constraint("x2 >= 0")
+    lp_problem.add_range_constraint("x1 <= 1")
+    lp_problem.add_range_constraint("x3 >= 0")
+    lp_problem.add_range_constraint("x4 >= 0")
+    
+    # print(lp_problem.get_problem())
+    lp_problem.solve()
+
+    print(lp_problem.get_solution())
+    print(lp_problem.get_optimal_value())
+    print(lp_problem.get_status())
---
+++ CODE/web.py
@@ -0,0 +1,104 @@
+import streamlit as st
+from PIL import Image
+from utils import *
+from simplex import *
+
+img = Image.open("images/calculator_icon.png")
+st.set_page_config(page_title="Linear Programming Solver", page_icon=img)
+
+def get_user_input():
+    st.header('Define the problem')
+
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        num_variables = st.number_input('Enter number of variables', min_value=1, max_value=50, value=1)
+    with col2:
+        num_constraints = st.number_input('Enter number of constraints', min_value=0, max_value=50, value=0)
+    with col3:
+        num_ranges = st.number_input('Enter number of ranges', min_value=0, max_value=50, value=0)
+    lp_problem = LPProblem(num_variables, num_constraints, num_ranges)
+
+    st.divider()
+    st.header('Define the objective function')
+    target_func = st.text_input('Enter objective function')
+    lp_problem.set_objective(target_func)
+
+    if num_constraints > 0:
+        st.divider()
+        st.header('Define the constraints')
+        for i in range(num_constraints):
+            constraint = st.text_input('Enter constraint ' + str(i + 1))
+            lp_problem.add_constraint(constraint)
+
+    if num_ranges > 0:
+        st.divider()
+        st.header('Define the range constraints')
+        for i in range(num_ranges):
+            range_constraint = st.text_input('Enter range constraint ' + str(i + 1))
+            lp_problem.add_range_constraint(range_constraint)   
+
+    return lp_problem
+
+
+def solve(problem):
+    problem.solve()
+    return problem.get_solution(), problem.get_optimal_value(), problem.get_status()
+
+
+def print_solution(solution, optimal_value, status):
+    try:
+        st.success("Problem solved!")
+    except:
+        st.error("Problem not solved!")
+        return
+    if solution is None:
+        st.write('No solution')
+    else:
+        st.write('Solution: ')
+        up_cols = st.columns(len(solution))
+        for i, (variable, value) in enumerate(solution.items()):
+            with up_cols[i]:
+                st.write(variable, '= ', round(value, 3))
+    if optimal_value is None:
+        st.write('No optimal value')
+    else:
+        st.write('Optimal value: ', round(optimal_value, 3))
+    st.write('Status: ', status)
+
+
+if __name__ == "__main__":
+    st.title('LINEAR PROGRAMMING SOLVER WEB APP')
+
+    with st.sidebar:
+        st.image("images/logo_khtn.png", width=300)
+        st.header('AUTHORS')
+        st.divider()
+        st.write("- Thi Ngọc Phúc Hậu - 19110313 \n" + 
+                "- Quách Phong Dương - 20280022 \n" +
+                "- Nguyễn Lê Ngọc Duy - 20280023 \n" +
+                "- Trần Lê Minh - 20280066 \n")
+
+    user_guide_tab, main_tab = st.tabs(["User guide", "Solver"])
+
+    with user_guide_tab:
+        st.write("0. Click on the Solver tab to start solving the problem. \
+                 Click on the User guide tab to see the user guide.")
+        st.image("images/section_0.png", width=650)
+        st.write("1. Enter number of variables, constraints and ranges, for example:")
+        st.image("images/section_1.png", width=650)
+        st.write("2. Enter the objective function (the signs and the coefficients must be seperated by a space) as the same as the example:")
+        st.image("images/section_2.png", width=650)
+        st.write("3. Enter the constraints (the signs and the coefficients must be seperated by a space) as the same as the example:")
+        st.image("images/section_3.png", width=650)
+        st.write("4. Enter the range constraints (the signs and the coefficients must be seperated by a space) as the same as the example:")
+        st.image("images/section_4.png", width=650)
+        st.write("5. Click on the Solve button to see the result:")
+        st.image("images/section_5.png", width=650)
+
+    with main_tab:
+        problem = get_user_input()
+        st.divider()
+        st.header("Result of the problem")
+        if st.button("Solve"):
+            solution, optimal_value, status = solve(problem)
+            print_solution(solution, optimal_value, status)
